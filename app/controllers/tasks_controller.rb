a1e76a3bff8ca326d1cf53a3b950bbd9f670cb66
penpen09 <ar-el.mbl.10@outlook.jp>
2020-08-31 16:49:27 +0900

    CRUD機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,53 @@
 class TasksController < ApplicationController
+  befor_action :set_task, only: [:shoe, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice:"task追加"
+      else
+        render :new
+      end
+    end
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+    @task = Task.find(params[:id])
+    if params[:back]
+      render  :edit
+    else
+      if @task.update(task_params)
+        redirect_to tasks_path, notice:"編集しました"
+      else
+        render :edit
+      end
+    end
+  end
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
+  end
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"削除しました"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
