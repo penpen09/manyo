3fba4b9aeb321a1a97c22ec4a391393ab73a3a67
penpen09 <ar-el.mbl.10@outlook.jp>
2020-09-13 22:58:38 +0900

    add:ラベルモデル、コントローラ、ビューの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_055539) do
+ActiveRecord::Schema.define(version: 2020_09_13_135525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
